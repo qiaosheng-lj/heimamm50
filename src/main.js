bdabf51a23987eb4b7460d902c1ec98914b36b54
qiaosheng-lj <63383343+qiaosheng-lj@users.noreply.github.com>
2020-05-16 16:02:38 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import axios from 'axios'
 
 Vue.config.productionTip = false
+
+// 导入less
+import '@/styles/base.less'
 
 new Vue({
   render: h => h(App),
